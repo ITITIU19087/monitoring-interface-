435568cc87c3ec9a82131266dd97bf6c35470bdb
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2024-02-08 01:23:47 +0700

    working version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import OrderChart from './OrderChart';
 import CandlestickChart from './CandlestickChart';
-import NewOrderChart from './NewOrderBarChart';
 
 function App() {
   return (
     <div className="App">
-      {/* <CandlestickChart /> */}
-      <NewOrderChart/>
+      <CandlestickChart />
+      <OrderChart/>
     </div>
   );
 }
